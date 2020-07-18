bbfd7193030bb200f478c3780b745663849bdb60
Senko <daromar2008@gmail.com>
2020-07-18 13:03:36 +0000

    fixed pinger

--- index.js
+++ index.js
@@ -46,7 +46,8 @@ fs.readdir("./commands/", (err, files) => {
   console.log("Loaded commands!");
 });
 
-nst express = require('express');
+const http = require("https");
+const express = require('express');
 const app = express();
 app.get("/", (request, response) => {
     response.sendStatus(200);
@@ -54,7 +55,7 @@ app.get("/", (request, response) => {
 
 app.listen(process.env.PORT);
 setInterval(() => {
-    http.get(`http://hbotplus.juangenial452.repl.co/`);
+    http.get(`http://gasbot.superchupu.repl.co/`));
 }, 280000);
 
 client.login(process.env.TOKEN);
