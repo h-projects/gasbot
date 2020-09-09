ecf640baa5bc76f47c0ead0cff91e78b497868f9
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2020-09-09 17:53:53 +0200

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
     .setTitle("__**Info**__")
     .addField("G.A.S Bot", "G.A.S Bot was created to defeat the letter G.")
     .addField("G Removal", "By default, it removes standalone G, but by usinq `h!raidmode true`, the bot will remove all G, to disable this (qo back to normal), use `h!raidmode false`.", false)
-    .addField("Code", `The bot is written in Discord.JS and hosted on Glitch, if you see any buqs, please tell us in the [Support Server](${client.config.serverInvite})!`, false)
+    .addField("Code", `The bot is written in Discord.JS and hosted on Linux, if you see any buqs, please tell us in the [Support Server](${client.config.serverInvite})!`, false)
     .addField("Commands", "For a list of commands, use `h!help`.", false)
     .addField("Why?", "Because G is evil and must be destroyed.", false)
     .setThumbnail(`${client.config.botLogo}`)
