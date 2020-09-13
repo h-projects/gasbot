269ef5712c16f705ed3318d302e2a11530b818de
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2020-09-13 18:38:36 +0200

    Added server loqs

---
+++ actions/guildCreate.js
@@ -0,0 +1,17 @@
+module.exports = async (before, after) => {
+
+    // Joined loq
+    client.on("guildCreate", guild => {
+    let aytchLoqs = client.channels.cache.get("754681399499423764");
+    let joinedEmbed = new client.disc.MessageEmbed()
+            .setFooter("G.A.S Bot", `${client.config.botLogo}`)
+            .setThumbnail(`${guild.iconURL()}`)
+            .setTimestamp()
+            .setColor("E74C3C")
+            .setTitle(`Joined __${guild.name}__!`)
+            .setDescription(`Just joined a server with **${guild.memberCount}** members!\nI'm in **${client.guilds.cache.size}** servers now!`)
+    
+            aytchLoqs.send(joinedEmbed);
+    });
+    
+};    
---
+++ actions/guildDelete.js
@@ -0,0 +1,17 @@
+module.exports = async (before, after) => {
+
+    // Left loq
+    client.on("guildDelete", guild => {
+    let aytchLoqs = client.channels.cache.get("754681399499423764");
+    let leftEmbed = new client.disc.MessageEmbed()
+            .setFooter("G.A.S Bot", `${client.config.botLogo}`)
+            .setThumbnail(`${guild.iconURL()}`)
+            .setTimestamp()
+            .setColor("E74C3C")
+            .setTitle(`Left ${guild.name}`)
+            .setDescription(`Just left a server with ${guild.memberCount} members\nI'm in ${client.guilds.cache.size} servers now`)
+    
+            aytchLoqs.send(leftEmbed);
+    });
+        
+};
