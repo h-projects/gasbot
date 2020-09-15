3671d631468d84dcac6530a1f5b6128bdb219e42
Dawid <39349444+Indexerrowaty@users.noreply.github.com>
2020-09-15 23:40:02 +0200

    Update raidmode.js

--- commands/raidmode.js
+++ commands/raidmode.js
@@ -1,18 +1,26 @@
-const fs = require("fs");
-const db = require("../database/raidmode.json");
+let fs = require("fs");
 
 exports.run = (client, message, args) => {
-  if (client.disc.member.hasPermission("ADMINSTRATOR")) {
-    return;
-  } else
-    return message.reply(
-      {
-        embed: {
-          color: 15158332,
-          title: '403 Forbidden',
-          description: 'You do not have permission to use that command!'
-        }
-      }
-    );
-  //   this would prob add the server id to the json and also remove it
+  if (!client.disc.member.hasPermission("ADMINSTRATOR"))
+    return message.channel.send("You do not have permission to use that command!");
+  
+  function saveJSON() {
+		let stringifiedJSON = JSON.stringify(client.raidmode);
+    fs.writeFile("./database/raidmode.json", stringifiedJSON, function(err) {
+      if (err) return console.log(err);
+    };
+  };
+  
+  if (!client.raidmode[message.guild.id]) {
+    client.raidmode[message.guild.id] = 0; saveJSON();
+  }
+    
+  if(!args[0] > 2 || !args[0] < 0) {
+    client.raidmode[message.guild.id] = Number(args[0]);
+    saveJSON();
+    return message.channel.send(`Successfully set protection level to **${args[0]}**!`);
+  }
+    
+  message.channel.send("G-protection levels: \n0 = Low - Unrecommended. You are almost not protected from G. \n1 = Medium - Not fully protected but still. Any protection is qood. \n3 = Hiqh - We quarantee you that no G will ever appear on your server! \n\n**Your current protection level: " + client.raidmode[message.guild.id] + "**");
+    
 };
