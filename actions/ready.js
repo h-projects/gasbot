c8d700751abf6b67bd8cf9c462a27f37533afa2d
Dawid <39349444+Indexerrowaty@users.noreply.github.com>
2020-09-27 13:29:07 +0200

    Update ready.js

--- actions/ready.js
+++ actions/ready.js
@@ -1,11 +1,11 @@
 module.exports = async () => {
   
   client.user.setPresence({
-    status: "dnd",
     activity: {
       name: `${client.config.prefix}help | Removinq G!`,
       type: "PLAYING"
-    }
+    },
+    status: "dnd"
   });
   
   console.log("Bot is now H");
