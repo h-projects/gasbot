1f76c43ac012f076b5dfb66b01f9d160c421114c
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2020-09-27 12:20:54 +0100

    Status Buqfix
    There was a buq that makes the status dissapear sometimes, hopefully fixed now

--- actions/ready.js
+++ actions/ready.js
@@ -1,4 +1,13 @@
 module.exports = async () => {
   
+  client.user.setPresence({
+    status: `dnd`,
+    activity: {
+      name: `${client.config.prefix}help | Removinq G!`,
+      type: "PLAYING"
+    }
+  });
+  
   console.log("Bot is now H");
+  
 };
