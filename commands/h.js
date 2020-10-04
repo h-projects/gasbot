6a59a70dc8c6ae26abfc54941aa6898424ddf799
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2020-10-04 12:24:21 +0100

    Update h.js

--- commands/h.js
+++ commands/h.js
@@ -1,10 +1,10 @@
 exports.run = (client, message, args) => {
   if (args.join(" ")) return;
-  message.channel.send("h = H, #bothhmatter", {
+  message.channel.send("h", {
     files: [
       {
         attachment:
-          "https://cdn.glitch.com/c5ebb36a-c4ea-424f-a0e6-f74d97f03143%2F647484b2-833c-4a02-9ea4-764ab2d9e0db.image.png?v=1591903836599",
+          "https://cdn.discordapp.com/emojis/699995508810317895.gif",
         name: `GAS_Bot-H-${message.id}.png`
       }
     ]
