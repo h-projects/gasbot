c8575c736bb20a834ebfdb1a06afa15959f5fe6d
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2020-10-04 12:15:13 +0100

    Update loqs.js

--- commands/loqs.js
+++ commands/loqs.js
@@ -1,11 +1,11 @@
 exports.run = (client, message, args) => {
   if (args.join(" ")) return;
-   let infoEmbed = new client.disc.MessageEmbed()
+   let loqsHelpEmbed = new client.disc.MessageEmbed()
     .setColor("E74C3C")
-    .setTitle("__**Loqs**__")
-    .addField('To enable loqs on your server, just create a channel called "loqs"!', "\u200b")
+    .setTitle("__Loqs__")
+    .setDescription("To enable loqs on your server, just create a channel called `loqs`!")
     .setTimestamp()
     .setFooter(`${message.author.tag}`, `${message.author.avatarURL()}`);
 
-  message.channel.send(infoEmbed);
+  message.channel.send(loqsHelpEmbed);
 };
