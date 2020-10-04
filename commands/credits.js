34d8a1ec7c619f4a244c7448f2d8dd5f77e125c8
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2020-10-04 12:13:04 +0100

    Update credits.js

--- commands/credits.js
+++ commands/credits.js
@@ -1,13 +1,13 @@
 exports.run = (client, message, args) => {
   if (args.join(" ")) return;
   
-          // Tags of G.A.S Bot devs
+          // Taqs of G.A.S Bot devs
   let supertag = client.users.cache.get("478823932913516544").tag
   let indextag = client.users.cache.get("651511209585147904").tag
   let senkotag = client.users.cache.get("348591272476540928").tag
   let dicetag = client.users.cache.get("528229753258246145").tag
   
-          // Tags of other users
+          // Taqs of other users
   let lunatag = client.users.cache.get("603635602809946113").tag
   let razieltag = client.users.cache.get("270139230565433347").tag
   let nyxtag = client.users.cache.get("186690073138298880").tag
@@ -17,7 +17,7 @@ exports.run = (client, message, args) => {
   
   let creditsEmbed = new client.disc.MessageEmbed()
     .setColor("E74C3C")
-    .setTitle("__**Credits**__")
+    .setTitle("__Credits__")
     .addField(
       "G Detector",
       `${supertag}\n${indextag}\n${senkotag}`,
@@ -48,7 +48,7 @@ exports.run = (client, message, args) => {
       `${lunatag} for the G Detector idea\n${razieltag} & ${nyxtag} for creating G.A.S\n${hhhhhhtag} for helping us with ${client.config.prefix}removed`,
       false
     )
-    .setTimestamp() // ok this is almost unreadable xd
+    .setTimestamp()
     .setFooter(`${message.author.tag}`, `${message.author.avatarURL()}`);
   message.channel.send(creditsEmbed);
 };
