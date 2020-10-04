93d1d290d9e3ab4e15d45b89dc9d8b1cd7c17df2
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2020-10-04 12:12:05 +0100

    Update links.js

--- commands/links.js
+++ commands/links.js
@@ -3,26 +3,26 @@ exports.run = (client, message, args) => {
   message.channel.send({
     embed: {
       color: 15158332,
-      title: "__**Links**__",
+      title: "__Links__",
       fields: [
         {
-          name: "**Want to remove <:NoSeventhLetter:721649657146769449> in your server?**",
+          name: "Want to remove <:NoSeventhLetter:721649657146769449> in your server?",
           value: `<:gas:734396942653718561> Invite the bot [here](${client.config.botInvite})`
         },
         {
-          name: "**Want to support G.A.S Bot?**",
+          name: "Want to support G.A.S Bot?",
           value: `<:gas:734396942653718561> Upvote the bot [here](${client.config.botUpvote})`
         },
         {
-          name: "**Need Help?**",
+          name: "Need Help?",
           value: `<:AytchSoftware:720949593696894996> Join the official Aytch Software Server [here](${client.config.serverInvite})`
         },
         {
-          name: "**Need help settinq up the bot?**",
+          name: "Need help settinq up the bot?",
           value: `<:AytchSoftware:720949593696894996> Read the docs [here](${client.config.docs})`
         },
         {
-          name: "**Do You hate <:NoSeventhLetter:721649657146769449>?**",
+          name: "Do You hate <:NoSeventhLetter:721649657146769449>?",
           value: `<:gas:734396942653718561> Join the official G.A.S Server [here](${client.config.gasServer})`
         }
       ],
