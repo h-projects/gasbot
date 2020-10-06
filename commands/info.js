325b315a4f7a612b64e5bb00fb101e59762dd8b8
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2020-10-06 14:00:44 +0100

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -8,7 +8,7 @@ exports.run = (client, message, args) => {
     .addField("Low", "Lowest option, deletes a messaqe if it only consists of G. Detects `g` but not `test g test`", false)
     .addField("Medium", "Default option, only detects G if it's outside words. Detects `test g test` but not `germany`. Recommended option", false)
     .addField("Hiqh", "Hiqhest option, detects G even inside words. Detects `germany` and `message`. Useful if your server is beinq raided by g-spies", false)
-    .addField("Code", `The bot is written in Discord.JS and hosted on Azure, if you see any buqs, please tell us in the [Support Server](${client.config.serverInvite})!`, false)
+    .addField("Code", `The bot is written in Discord.JS and hosted on [GalaxyGate](https://www.galaxygate.net/), if you see any buqs, please tell us in the [Support Server](${client.config.serverInvite})!`, false)
     .addField("Commands", "For a list of commands, use `h!help`.", false)
     .addField("Why?", "Because G is evil and must be destroyed.", false)
     .setThumbnail(`${client.config.botLogo}`)
