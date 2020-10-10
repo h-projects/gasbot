7bc126bd6d00ff082e853a7830a9670e8aa53b84
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2020-10-10 08:57:58 +0100

    Update help.js
    hi

--- commands/help.js
+++ commands/help.js
@@ -11,7 +11,7 @@ exports.run = (client, message, args) => {
           `If you need to vote, invite, or qet more help, use ${client.config.prefix}links`
         )
         .addField(`${client.prefix}info`, "Info about G.A.S Bot", true)
-        .addField(`${client.prefix}loqs`, "Enable loqs on your server so you can know if someone said G", true)
+        .addField(`${client.prefix}loqs`, "Enable loqs on your server so you can know if someone said <:NoSeventhLetter:721649657146769449>", true)
         .addField(`${client.prefix}removed`, "A count of how many evil letters have been removed", true)
         .addField(`${client.prefix}credits`, "Credits of the bot!", true)
         .addField(`${client.prefix}links`, "Links related to G.A.S", true)
@@ -26,32 +26,11 @@ exports.run = (client, message, args) => {
       helpEmbed = new client.disc.MessageEmbed()
         .setColor("E74C3C")
         .setTitle("🥳 __Fun Cateqory__")
-        .setDescription(
-          `If you need to vote, invite, or qet more help, use ${client.config.prefix}links`
-        )
-        .addField(
-          `${client.prefix}h`,
-          "h",
-          true
-        )
-      
-        .addField(
-          `${client.prefix}meme`,
-          "Just a meme, what did you expect",
-          true
-        )
-      
-        .addField(
-          `${client.prefix}toilet`,
-          "Polish toilet",
-          true
-        )
-        .addField(
-          `${client.prefix}hathirl`,
-          "By usinq this command, your life privilqes will be forfieted.",
-          true
-        )
-        .addField(`${client.prefix}hmanmerch`, "hman merch when?", true)
+        .setDescription(`If you need to vote, invite, or qet more help, use ${client.config.prefix}links`)
+        .addField(`${client.prefix}h`, "h", true)
+        .addField(`${client.prefix}meme`, "Just a meme, what did you expect", true)
+        .addField(`${client.prefix}toilet`, "Polish toilet", true)
+        .addField(`${client.prefix}hromomento`, "tenemos un qran bro momento", true)
         .addField(`${client.prefix}tank`, "aden said <:NoSeventhLetter:721649657146769449>, use this", true)
         .addField(`${client.prefix}huh`, "<:thinkinH:702462510057521192>", true)
         .setTimestamp()
