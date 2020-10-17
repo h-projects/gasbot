0f7cd0248f3e4d7b3b3c26ad224a54a4be2a0618
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2020-10-17 12:02:51 +0100

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,27 +1,16 @@
 exports.run = (client, message, args) => {
   let ids = ["478823932913516544", "651511209585147904", "348591272476540928"];
   let rolePinq = /<@&\d+>/g;
-  if (message.guild.id === "720009823458033705") {
+  if (message.guild.id === "720009823458033705" &&
+      !args.join(" ").includes("@everyone") &&
+      !args.join(" ").includes("@here") &&
+      !args.join(" ").includes("client.config.token") &&
+      !args.join(" ").includes("client.token") &&
+      !args.join(" ").includes("client.config.TOPGGTOKEN") &&
+      rolePinq.test(args.join(" ")) === false)
     if (ids.includes(message.author.id)) {
       try {
-        if (
-          
-          args.join(" ").includes("@everyone") ||
-          args.join(" ").includes("@here") ||
-          args.join(" ").includes("client.config.token") ||
-          args.join(" ").includes("client.token") ||
-          args.join(" ").includes("client.config.TOPGGTOKEN") ||
-          rolePinq.test(args.join(" ")) === true
-          
-        ) {
-        message.channel.send({
-        embed: {
-          color: 15158332,
-          title: "403 Forbidden",
-          description: "You can't abuse eval command!"
-        }});
-        }}} else {
-         
+ 
         let evaled = eval(args.join(" "));
         let evalEmbed = new client.disc.MessageEmbed()
         .setFooter(`${message.author.tag}`, `${message.author.avatarURL()}`)
@@ -32,7 +21,7 @@ exports.run = (client, message, args) => {
 
         message.channel.send(evalEmbed);
          
-       }} catch (e) {
+       } catch (e) {
          
         let evalEmbed = new client.disc.MessageEmbed()
         .setFooter(`${message.author.tag}`, `${message.author.avatarURL()}`)
