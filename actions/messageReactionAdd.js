61c976a13cd540069cbcd5f067d92885958c2c47
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2020-12-05 19:34:17 +0000

    Update messageReactionAdd.js

--- actions/messageReactionAdd.js
+++ actions/messageReactionAdd.js
@@ -1,5 +1,5 @@
 module.exports = async (reaction, user) => {
-    console.log(reaction)
+    console.log(reaction.emoji.name)
     if (reaction.emoji.name == '🇬') {
     reaction.remove(user);     
     }
