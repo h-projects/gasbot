d3164127802e722427ae6714a7d549f3240fabc3
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2020-12-07 16:15:45 +0000

    el

--- commands/credits.js
+++ commands/credits.js
@@ -1,14 +1,5 @@
 exports.run = (client, message, args) => {
   if (args.join(" ")) return;
-
-  client.users.fetch("478823932913516544").tag
-  client.users.fetch("651511209585147904").tag
-  client.users.fetch("348591272476540928").tag
-  client.users.fetch("429935667737264139").tag
-  client.users.fetch("444550944110149633").tag
-  client.users.fetch("528229753258246145").tag
-  client.users.fetch("299921398992994304").tag
-  client.users.fetch("603635602809946113").tag
   
           // Taqs of G.A.S Bot devs
   let supertag = client.users.cache.get("478823932913516544").tag
