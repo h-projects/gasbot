0d15172589fef221039e719c3aecd431d23afd20
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2020-12-07 16:16:24 +0000

    th

--- actions/ready.js
+++ actions/ready.js
@@ -1,5 +1,12 @@
-module.exports = async () => {
-  
+module.exports = async (client) => {
+  client.users.fetch("478823932913516544").tag
+  client.users.fetch("651511209585147904").tag
+  client.users.fetch("348591272476540928").tag
+  client.users.fetch("429935667737264139").tag
+  client.users.fetch("444550944110149633").tag
+  client.users.fetch("528229753258246145").tag
+  client.users.fetch("299921398992994304").tag
+  client.users.fetch("603635602809946113").tag
   console.log("Bot is now H");
   
 };
