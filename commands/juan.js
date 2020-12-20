906480f32c3b40d2ca3d686ec7b0e26b8d59092a
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2020-12-20 11:22:41 +0100

    Content was uploaded to .gif, not to .png

--- commands/juan.js
+++ commands/juan.js
@@ -7,7 +7,7 @@ exports.run = (client, message, args) => {
         {
           attachment:
             "https://cdn.discordapp.com/attachments/729730142125031505/768799803009269771/hYrZ6QK.png",
-          name: `GAS_Bot-Juan-${message.id}.gif`
+          name: `GAS_Bot-Juan-${message.id}.png`
         }
       ]
     }
