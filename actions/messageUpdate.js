972c535a8db9323e722e63fa73f700fae640700c
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2020-12-22 11:49:33 +0100

    testeo

--- actions/messageUpdate.js
+++ actions/messageUpdate.js
@@ -4,7 +4,7 @@ module.exports = async (client, oldMessage, message) => {
     var HowMuchGWasPostedUser = require("../database/badLetterUser.json");
       
     if (message.partial) {
-      message.fetch()
+      await message.fetch()
     };
 	    
     // Go aways bots and people who are trying to use commands on dm
