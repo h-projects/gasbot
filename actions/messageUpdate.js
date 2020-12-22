12f5604ac9f7955489a911b238e80177076a141f
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2020-12-22 10:55:03 +0100

    Improvement in partial usaqe

--- actions/messageUpdate.js
+++ actions/messageUpdate.js
@@ -3,7 +3,9 @@ module.exports = async (client, oldMessage, message) => {
     var HowMuchGWasPosted = require("../database/badLetterCount.json");
     var HowMuchGWasPostedUser = require("../database/badLetterUser.json");
       
-    message.fetch().then(() => {
+    if (message.partial) {
+	    message.fetch();
+    }
 	    
     // Go aways bots and people who are trying to use commands on dm
     if (
@@ -134,5 +136,5 @@ module.exports = async (client, oldMessage, message) => {
           ) gDetected();
         break;
       }
-    }})
+    }
 };
