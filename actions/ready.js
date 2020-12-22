1d96f26a49464bf35727760c1b30d837c885d0ad
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2020-12-22 09:52:17 +0100

    Improve fetchinq h!credits users

--- actions/ready.js
+++ actions/ready.js
@@ -1,13 +1,13 @@
 module.exports = async (client) => {
-  client.users.fetch("478823932913516544").tag
-  client.users.fetch("651511209585147904").tag
-  client.users.fetch("348591272476540928").tag
-  client.users.fetch("429935667737264139").tag
-  client.users.fetch("444550944110149633").tag
-  client.users.fetch("528229753258246145").tag
-  client.users.fetch("299921398992994304").tag
-  client.users.fetch("692037827940057129").tag
-  client.users.fetch("603635602809946113").tag
+  client.users.fetch("478823932913516544")
+  client.users.fetch("651511209585147904")
+  client.users.fetch("348591272476540928")
+  client.users.fetch("429935667737264139")
+  client.users.fetch("444550944110149633")
+  client.users.fetch("528229753258246145")
+  client.users.fetch("299921398992994304")
+  client.users.fetch("692037827940057129")
+  client.users.fetch("603635602809946113")
   console.log("Bot is now H");
   
 };
