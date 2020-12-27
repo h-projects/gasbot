18963b85da78c7c2e65e511e11907331006e2828
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2020-12-27 13:10:53 +0100

    Nickname Detector Test

--- actions/guildMemberUpdate.js
+++ actions/guildMemberUpdate.js
@@ -2,6 +2,8 @@ module.exports = async (after, before) => {
   if (after.nickname === undefined || after.nickname === null || after.nickname === "") return;
   let upperCaseNick = after.nickname.toUpperCase();
   let lowDetection = /[^\sG]/gi;
+  console.log(`after.nickname: ${after.nickname}`)
+  console.log(`before.nickname: ${before.nickname}`)
 
   if (lowDetection.test(upperCaseNick) === false) {
     let newNickname = before.nickname
