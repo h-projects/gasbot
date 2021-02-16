4462822e9709e21f9ef7a8bf72f3a8b20d442140
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2021-02-16 21:23:10 +0000

    please work

--- actions/guildMemberUpdate.js
+++ actions/guildMemberUpdate.js
@@ -6,8 +6,8 @@ module.exports = async (client, oldMember, newMember) => {
   if (lowDetection.test(newMember.nickname) === false) {
     
     let newNickname = newMember.nickname
-      .replaceAll(/[gḡᵷ𝔤𝖌𝐠𝘨𝙜𝚐𝕘𝗀𝗴ɡ𝘨ℊ𝗚ᧁɓ⅁ᏵᏀᏳ𝓰𝐠ᴳ❡𝙶🄶𝙂𝒢🇬ᶃꓖ𝖦Ꮆʛ𝘎Ⴚｇ🅶𝓖🅖𝔾𝔊ꞡ𝕲𝑔ģ𝐆ƍ𝐺𝑮Ġ𝒈ꮐԍg̵ɢǵᏻց𝚐ⒼƃᘜＧᘜƓɢᶢᵍ₲ꍌꁅĜǧĞǤᕤᘓ𝞋𝟅᠖ᡋᠪ໔]+/gi, "h");
-    newMember.setNickname(newNickname);
+      .replace(/[gḡᵷ𝔤𝖌𝐠𝘨𝙜𝚐𝕘𝗀𝗴ɡ𝘨ℊ𝗚ᧁɓ⅁ᏵᏀᏳ𝓰𝐠ᴳ❡𝙶🄶𝙂𝒢🇬ᶃꓖ𝖦Ꮆʛ𝘎Ⴚｇ🅶𝓖🅖𝔾𝔊ꞡ𝕲𝑔ģ𝐆ƍ𝐺𝑮Ġ𝒈ꮐԍg̵ɢǵᏻց𝚐ⒼƃᘜＧᘜƓɢᶢᵍ₲ꍌꁅĜǧĞǤᕤᘓ𝞋𝟅᠖ᡋᠪ໔]+/gi, "h");
+      newMember.setNickname(newNickname);
     
   }
 };
