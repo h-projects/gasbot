556904bcec4b404c1e15cc915472a3572aba40bf
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2021-03-25 14:04:57 +0000

    Nevermind i know what went wronq

--- gasbot.js
+++ gasbot.js
@@ -1,5 +1,6 @@
-const Discord = require("discord.js"),
-  client = new Discord.Client({
+// Setup Discord Client
+const Discord = require("discord.js");
+const client = new Discord.Client({
     ws: {
       intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS', 'GUILD_MEMBERS']
     },
@@ -9,30 +10,29 @@ const Discord = require("discord.js"),
       activity: { name: "h!help | Removinq G!", type: "PLAYING" },
       status: "dnd"
     }
-  }),
-  DBL = require("dblapi.js"),
-  jquery = require("jquery"),
-  fs = require("fs-extra"),
-  enmap = require("enmap");
+ });
+
+// Config related requires
 client.disc = require("discord.js");
 client.config = require("./database/config.json");
-const dbl = new DBL(client.config.TOPGGTOKEN, client);
-client.raidmode = require("./database/raidmode.json");
 client.prefix = require("./database/prefix.json");
+
+// Setup external libraries
+const DBL = require("dblapi.js");
+const jquery = require("jquery");
+const fs = require("fs-extra")
+const enmap = require("enmap");
+const dbl = new DBL(client.config.TOPGGTOKEN, client);
+
+// Database related requires
+client.raidmode = require("./database/raidmode.json");
 client.loqs = require("./database/loqs.json");
 client.badLetterCount = require("./database/badLetterCount.json");
 client.badLetterUser = require("./database/badLetterUser.json");
 client.badLetterGuild = require("./database/badLetterGuild.json");
 client.restartID = require("./database/restart.json");
-client.statuses = [
-  "Removinq G!",
-  "Don't use the bad letter",
-  "H Clicker",
-  "Removed " + client.badLetterCount.badLetterCount + " G's so far"
-];
 
-process.on("unhandledRejection", e => console.error(`Error: ${e}`));
-
+// Load actions
 fs.readdir("./actions/", (err, files) => {
   console.log("Loading actions...");
   if (err) return console.error(err);
@@ -45,6 +45,7 @@ fs.readdir("./actions/", (err, files) => {
   console.log("Loaded actions!");
 });
 
+// Load commands
 client.cmds = new enmap();
 fs.readdir("./commands/", (err, files) => {
   console.log("Loading commands...");
@@ -59,4 +60,6 @@ fs.readdir("./commands/", (err, files) => {
   console.log("Loaded commands!");
 });
 
+// Error handlinq + loqqinq in
+process.on("unhandledRejection", e => console.error(`Error: ${e}`));
 client.login(client.config.token);
