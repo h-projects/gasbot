292e0ae730e20ebe4fecebc51ffd87c074d737c4
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2021-04-19 15:45:20 +0100

    Nickname Detector

--- actions/guildMemberUpdate.js
+++ actions/guildMemberUpdate.js
@@ -1,16 +1,19 @@
 module.exports = async (client, oldMember, newMember) => {
 
-  if (newMember.partial) { await newMember.fetch() };
+  if (newMember.partial) { await newMember.fetch(); };
 
-  if (newMember.nickname === undefined || newMember.nickname === null || newMember.nickname === "") { return; }
-  let lowDetection = /[^\sgḡᵷ𝔤𝖌𝐠𝘨𝙜𝚐𝕘𝗀𝗴ɡ𝘨ℊ𝗚ᧁɓ⅁ᏵᏀᏳ𝓰𝐠ᴳ❡𝙶🄶𝙂𝒢🇬ᶃꓖ𝖦Ꮆʛ𝘎Ⴚｇ🅶𝓖🅖𝔾𝔊ꞡ𝕲𝑔ģ𝐆ƍ𝐺𝑮Ġ𝒈ꮐԍg̵ɢǵᏻց𝚐ⒼƃᘜＧᘜƓɢᶢᵍ₲ꍌꁅĜǧĞǤᕤᘓ𝞋𝟅᠖ᡋᠪ໔]/gi;
+  if (newMember.nickname === undefined || newMember.nickname === null || newMember.nickname === "" || !newMember.guild.me.hasPermission("MANAGE_NICKNAMES")) { return; }
 
 
-  if (lowDetection.test(newMember.nickname) === false) {
+  let detection = /[gḡᵷ𝔤𝖌𝐠𝘨𝙜𝚐𝕘𝗀𝗴ɡ𝘨ℊ𝗚ᧁɓ⅁ᏵᏀᏳ𝓰𝐠ᴳ❡𝙶🄶𝙂𝒢🇬ᶃꓖ𝖦Ꮆʛ𝘎Ⴚｇ🅶𝓖🅖𝔾𝔊ꞡ𝕲𝑔ģ𝐆ƍ𝐺𝑮Ġ𝒈ꮐԍg̵ɢǵᏻց𝚐ⒼƃᘜＧᘜƓɢᶢᵍ₲ꍌꁅĜǧĞǤᕤᘓ𝞋𝟅᠖ᡋᠪ໔␝]/giu;
+  let cleanNickname = newMember.nickname.replace(/[.\-_ /\\()[\]]/gi, "")
+  let array = [...cleanNickname.matchAll(detection)];
+  let percentage = cleanNickname.length / array.length
 
-    let newNickname = newMember.nickname
-      .replace(/[gḡᵷ𝔤𝖌𝐠𝘨𝙜𝚐𝕘𝗀𝗴ɡ𝘨ℊ𝗚ᧁɓ⅁ᏵᏀᏳ𝓰𝐠ᴳ❡𝙶🄶𝙂𝒢🇬ᶃꓖ𝖦Ꮆʛ𝘎Ⴚｇ🅶𝓖🅖𝔾𝔊ꞡ𝕲𝑔ģ𝐆ƍ𝐺𝑮Ġ𝒈ꮐԍg̵ɢǵᏻց𝚐ⒼƃᘜＧᘜƓɢᶢᵍ₲ꍌꁅĜǧĞǤᕤᘓ𝞋𝟅᠖ᡋᠪ໔]/gi, "h");
+
+  if (percentage >= 0.75) {
+
+    let newNickname = newMember.nickname.replace(detection, "h");
     newMember.setNickname(newNickname);
-
   }
 };
