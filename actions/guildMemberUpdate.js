a6ec06139d4fbf420e685705068c63497ee8e77b
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2021-04-20 22:14:08 +0100

    feat(Nickname Detector): add loqqinq and polish the detector (#6)

--- actions/guildMemberUpdate.js
+++ actions/guildMemberUpdate.js
@@ -12,7 +12,109 @@ module.exports = async (client, oldMember, newMember) => {
 
   if (array.length / cleanNickname.length >= 0.75) {
 
+    // Replace and remove the detected nickname
     let newNickname = newMember.nickname.replace(detection, "h");
     newMember.setNickname(newNickname);
+
+
+
+    // Make the global removed count qo up    
+    client.badLetterCount.badLetterCount++;
+    fs.writeFile(
+      "./database/badLetterCount.json",
+      JSON.stringify(client.badLetterCount),
+      function (err) {
+        if (err) return console.error(`Somethinq qone G in updatinq how much G's was posted: ${err}`);
+      }
+    );
+
+    // Make the guild removed count qo up
+    if (client.badLetterGuild[message.guild.id] !== undefined) {
+
+      client.badLetterGuild[message.guild.id]++;
+
+    } else {
+
+      client.badLetterGuild[message.guild.id] = 1;
+
+    };
+
+    // Write the guild removed count
+    fs.writeFile(
+      "./database/badLetterGuild.json",
+      JSON.stringify(client.badLetterGuild),
+      function (err) {
+        if (err) return console.error(`Somethinq qone G in updatinq how much G's was posted with a quild: ${err}`);
+      }
+    );
+
+    // Make the user removed count qo up
+    if (client.badLetterUser[message.author.id] !== undefined) {
+
+      client.badLetterUser[message.author.id]++;
+
+    } else {
+
+      client.badLetterUser[message.author.id] = 1;
+
+    };
+
+    // Write the user removed count
+    fs.writeFile(
+      "./database/badLetterUser.json",
+      JSON.stringify(client.badLetterUser),
+      function (err) {
+        if (err) return console.error(`Somethinq qone G in updatinq how much G's was posted with an user: ${err}`);
+      }
+    );
+
+
+
+
+    // Find loqs channel
+    if (client.loqs[newMember.guild.id] !== undefined) {
+
+      var loqChannel = newMember.guild.channels.cache.get(client.loqs[newMember.guild.id]);
+
+      if (loqChannel == undefined) {
+        var loqChannel = newMember.guild.channels.cache.find(channel => channel.name === "loqs");
+      }
+
+    } else {
+      var loqChannel = newMember.guild.channels.cache.find(channel => channel.name === "loqs");
+    }
+
+
+    
+    let centralLoq = client.channels.cache.get("805472059790589974");
+
+    let loqEmbed = new client.disc.MessageEmbed()
+      .setFooter("G.A.S Bot", client.user.avatarURL({ dynamic: true }))
+      .setURL("https://h-projects.github.io/app/fuck-g/")
+      .setThumbnail(newMember.user.avatarURL({ dynamic: true }))
+      .setTimestamp()
+      .setColor("E74C3C")
+      .setTitle("G Removal")
+      .addField("Type", "Nickname")
+      .addField("User", `${newMember.user} (${newMember.user.id})`)
+      .addField("Nickname", newMember.nickname);
+
+    let centralLoqEmbed = new client.disc.MessageEmbed()
+      .setFooter("G.A.S Bot", client.user.avatarURL({ dynamic: true }))
+      .setURL("https://h-projects.github.io/app/fuck-g/")
+      .setThumbnail(newMember.user.avatarURL({ dynamic: true }))
+      .setTimestamp()
+      .setColor("E74C3C")
+      .setTitle("G Removal")
+      .addField("Type", "Nickname")
+      .addField("User", `${newMember.user.tag} (${newMember.user.id})`)
+      .addField("Server", `${newMember.guild} (${newMember.guild.id})`)
+      .addField("Nickname", newMember.nickname);
+
+    // Send loqs messaqe
+    if (loqChannel !== undefined && loqChannel.permissionsFor(client.user.id).has('SEND_MESSAGES')) { loqChannel.send(loqEmbed); };
+      if (newMember.guild.id != "805472058954874941") { centralLoq.send(centralLoqEmbed); };
+
+
   }
 };
