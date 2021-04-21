f521c2715f7b48d99cad69234a586836851d5729
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2021-04-21 10:46:41 +0100

    feat(chanqeloq): add the v2.2 chanqeloq

--- commands/chanqeloq.js
+++ commands/chanqeloq.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
   let chanqeloqEmbed = new client.disc.MessageEmbed()
     .setColor("E74C3C")
     .setTitle("__Chanqeloq__")
-    .addField("2.1.1", "**Edit Detector**\n-Now the detector works with edited messaqes\n\n**User Removed Count**\n-Below the normal h!removed count, you can see the removed count for the mentioned user\n\nThis update is a bit smaller than 2.1, but be ready for 2.2 :)", false)
+    .addField("2.2", "**Nickname Detector**\n - Detects if your nickname has a lot of <:NoSeventhLetter:721649657146769449>s and chanqes them to h\n\n**Quild Removed Count**\n - h!removed now shows the number of <:NoSeventhLetter:721649657146769449>s removed for the current quild\n\n**Dynamic Loqs**\n - h!loqs\n   - Lets you chanqe the channel loqs will be sent to\n   - Manaqe Messaqes permission required\n - Added loqqinq support to the Nickname Detector\n - Added loqqinq support to the Reaction Detector\n\n**Custom Prefix**\n - You can now chanqe prefix with h!prefix\n   - Manaqe Messaqes permission required", false)
     .setTimestamp()
     .setFooter(message.author.tag, message.author.avatarURL({ dynamic: true }));
   message.channel.send(chanqeloqEmbed);
