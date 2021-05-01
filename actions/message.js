b87ea8eb95622f757c791a97971e6f303fb5361a
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2021-05-01 13:56:31 +0100

    Revert "Remove spaces"
    This reverts commit b9c88a3d492f177359d5723a546acb17f1ed8114.

--- actions/message.js
+++ actions/message.js
@@ -82,10 +82,13 @@ module.exports = async (client, message, member) => {
 
       // Find loqs channel
       if (client.loqs[message.guild.id] !== undefined) {
+
         var loqChannel = message.guild.channels.cache.get(client.loqs[message.guild.id]);
+
         if (loqChannel == undefined) {
           var loqChannel = message.guild.channels.cache.find(channel => channel.name === "loqs");
         }
+
       } else {
         var loqChannel = message.guild.channels.cache.find(channel => channel.name === "loqs");
       }
@@ -119,8 +122,8 @@ module.exports = async (client, message, member) => {
 
       // Send loqs messaqe
       if (message.channel.permissionsFor(client.user.id).has('MANAGE_MESSAGES')) {
-        if (loqChannel !== undefined && loqChannel.permissionsFor(client.user.id).has('SEND_MESSAGES')) { loqChannel.send(loqEmbed); }
-        if (message.guild.id != "805472058954874941") { centralLoq.send(centralLoqEmbed); }
+        if (loqChannel !== undefined && loqChannel.permissionsFor(client.user.id).has('SEND_MESSAGES')) { loqChannel.send(loqEmbed); };
+        if (message.guild.id != "805472058954874941") { centralLoq.send(centralLoqEmbed); };
       }
     }
 
@@ -184,6 +187,7 @@ module.exports = async (client, message, member) => {
       .setColor("E74C3C")
       .setTitle("Prefix")
       .setDescription(client.prefix[message.guild.id]);
+    message.channel.send(helpEmbed);
   };
 
   // No prefix no fun
