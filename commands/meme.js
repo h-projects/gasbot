f3d2c1f6e43c0158b0cad1a0fde9420c971766b1
Sadie K. Brooklyn <senkodev@gmail.com>
2021-05-19 00:53:18 +0300

    added embeded meme

--- commands/meme.js
+++ commands/meme.js
@@ -1,12 +1,18 @@
 exports.run = async (client, message, args) => {
   if (args.join(" ")) return;
-  message.channel.send("Here is your meme!", {
-    files: [
-      {
-        attachment:
-          "https://api.xenith.pl/memapi",
-        name: `GAS_Bot-Meme-${message.id}.png`
-      }
-    ]
-  });
+
+  const meme = new client.disc.MessageAttachment("https://api.xenith.pl/memapi").setName(`GAS_Bot-Meme-${message.id}.png`)
+  let embed = new client.disc.MessageEmbed();
+
+  embed.setTitle('Here is your meme')
+  embed.setDescription('Provided by the Xenith MemeAPI.')
+
+  embed.attachFiles([meme])
+  embed.setImage(`attachment://GAS_Bot-Meme-${message.id}.png`)
+
+  embed.setColor("E74C3C")
+  embed.setTimestamp()
+  embed.setFooter(message.author.tag, message.author.avatarURL({ dynamic: true }))
+
+  message.channel.send(embed);
 }
