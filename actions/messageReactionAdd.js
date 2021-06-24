f50c61a0df4cf0f2d06856c24f0667be5f1474f7
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2021-06-24 23:01:18 +0100

    fix(messageReactionAdd): channel was undefined

--- actions/messageReactionAdd.js
+++ actions/messageReactionAdd.js
@@ -57,8 +57,8 @@ module.exports = async (client, reaction, user) => {
         );
         
         // Find loqs channel
-        let loqChannel = reaction.guild.channels.cache.get(client.loqs[reaction.guild.id]); 
-        loqChannel ??= reaction.guild.channels.cache.find(channel => channel.name === "loqs");
+        let loqChannel = reaction.message.guild.channels.cache.get(client.loqs[reaction.message.guild.id]); 
+        loqChannel ??= reaction.message.guild.channels.cache.find(channel => channel.name === "loqs");
       
         let centralLoq = client.channels.cache.get("805472059790589974");
 
