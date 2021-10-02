f4b5831fec4e44d4aa158dc0600110228efe2803
Superchupu <miguelgurriarannieto@gmail.com>
2021-10-02 21:28:39 +0100

    fix(detector): return false when no content

--- detector/levels/low.js
+++ detector/levels/low.js
@@ -1,4 +1,5 @@
 module.exports = content => {
   const badLetters = require('../detection.json').join('');
+  if (!content) return false;
   return !RegExp(`[^\\s${badLetters}]`, 'iu').test(content);
 };
