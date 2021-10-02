fb0c6cb068e7a185c03ea88529fc033d225abcf2
Superchupu <miguelgurriarannieto@gmail.com>
2021-10-02 14:14:42 +0100

    feat: add links command

---
+++ interactions/commands/links.js
@@ -0,0 +1,32 @@
+module.exports = {
+  name: 'links',
+  async execute(client, interaction) {
+    const nog = '<:nog:676105350306594819>';
+    const gas = '<:gas_new:855788595830194196>';
+    const aytchSoftware = '<:AytchSoftware:720949593696894996>';
+    interaction.reply({
+      embeds: [{
+        title: 'Links',
+        fields: [
+          {
+            name: `Want to remove ${nog} in your server?`,
+            value: `${gas} Invite the bot [here](${client.generateInvite(client.config.invite)})`
+          },
+          {
+            name: 'Want to support the bot?',
+            value: `⬆️ Upvote it [here](${client.config.vote})`
+          },
+          {
+            name: 'Need help?',
+            value: `${aytchSoftware} Join the Support Server [here](${client.config.support})`
+          },
+          {
+            name: `Do you hate ${nog}?`,
+            value: `${nog} Join the G Annihilation Squad [here](${client.config.gasServer})`
+          }
+        ],
+        color: client.config.color
+      }]
+    });
+  }
+};
