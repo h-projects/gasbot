d5f075f6954bed002a580ed7080ca1f3d10cdb65
Superchupu <miguelgurriarannieto@gmail.com>
2021-10-30 12:46:47 +0100

    fix(commands): return an error if it was thrown

--- commands/dev/commands.js
+++ commands/dev/commands.js
@@ -10,26 +10,30 @@ module.exports = {
     await client.application.commands.fetch();
 
     let response;
-    switch (action) {
-      case 'find':
-        response = client.application.commands.cache.find(c => c.name === args[1])?.id;
-        break;
+    try {
+      switch (action) {
+        case 'find':
+          response = client.application.commands.cache.find(c => c.name === args[1])?.id;
+          break;
 
-      case 'edit':
-        response = await client.application.commands.cache.get(json.shift()).edit(JSON.parse(json.join(' '))).catch(err => err);
-        break;
+        case 'edit':
+          response = await client.application.commands.cache.get(json.shift()).edit(JSON.parse(json.join(' '))).catch(err => err);
+          break;
 
-      case 'create':
-        response = await client.application.commands.create(JSON.parse(json.join(' '))).catch(err => err);
-        break;
+        case 'create':
+          response = await client.application.commands.create(JSON.parse(json.join(' '))).catch(err => err);
+          break;
 
-      case 'set':
-        response = await client.application.commands.set(JSON.parse(json.join(' '))).catch(err => err);
-        break;
+        case 'set':
+          response = await client.application.commands.set(JSON.parse(json.join(' '))).catch(err => err);
+          break;
 
-      case 'delete':
-        response = await client.application.commands.cache.get(args[1]).delete().catch(err => err);
-        break;
+        case 'delete':
+          response = await client.application.commands.cache.get(args[1]).delete().catch(err => err);
+          break;
+      }
+    } catch (e) {
+      response = e;
     }
 
     message.channel.send({
