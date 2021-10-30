bc8d4e5b52833a5d838d4f85996da8b8b3d97e87
Superchupu <miguelgurriarannieto@gmail.com>
2021-10-30 12:45:58 +0100

    refactor(interactions): use built-in methods to check the interaction type

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -4,63 +4,60 @@ module.exports = {
   async execute(interaction, client) {
     if (!interaction.inCachedGuild()) return;
 
-    switch (interaction.type) {
-      case 'APPLICATION_COMMAND':
-        const name = interaction.commandName;
-        const command = client.interactions.commands.get(name) ?? client.interactions.commands.find(cmd => cmd.contextMenu === name);
+    if (interaction.isApplicationCommand()) {
+      const name = interaction.commandName;
+      const command = client.interactions.commands.get(name) ?? client.interactions.commands.find(c => c.contextMenu === name);
 
-        if (!command) return;
+      if (!command) return;
 
-        if (!interaction.member.permissions.has(command.permissions ?? 0n)) {
-          return interaction.reply({
-            content: `You need the \`${command.permissions}\` permission to use this command`,
-            ephemeral: true
-          });
-        }
+      if (!interaction.member.permissions.has(command.permissions ?? 0n)) {
+        return interaction.reply({
+          content: `You need the \`${command.permissions}\` permission to use this command`,
+          ephemeral: true
+        });
+      }
 
-        if (!interaction.guild.me.permissions.has(command.botPermissions ?? 0n)) {
-          return interaction.reply({
-            embeds: [{
-              title: 'Missinq Permissions',
-              description: `I need the \`${command.botPermissions}\` permission to use this command`,
-              color: client.config.color
-            }],
-            ephemeral: true
-          });
-        }
+      if (!interaction.guild.me.permissions.has(command.botPermissions ?? 0n)) {
+        return interaction.reply({
+          embeds: [{
+            title: 'Missinq Permissions',
+            description: `I need the \`${command.botPermissions}\` permission to use this command`,
+            color: client.config.color
+          }],
+          ephemeral: true
+        });
+      }
 
-        command.execute(client, interaction);
-        break;
+      return command.execute(client, interaction);
+    }
 
+    if (interaction.isMessageComponent) {
+      [interaction.name, interaction.value, interaction.author] = interaction.customId.split(':');
+      const component = client.interactions.components.get(interaction.name);
 
-      case 'MESSAGE_COMPONENT':
-        [interaction.name, interaction.value, interaction.author] = interaction.customId.split(':');
-        const component = client.interactions.components.get(interaction.name);
+      if (!component || interaction.author !== interaction.user.id) {
+        return interaction.deferUpdate();
+      }
 
-        if (!component || interaction.author !== interaction.user.id) {
-          return interaction.deferUpdate();
-        }
+      if (!interaction.member.permissions.has(component.permissions ?? 0n)) {
+        return interaction.reply({
+          content: `You need the \`${component.permissions}\` permission to use this component`,
+          ephemeral: true
+        });
+      }
 
-        if (!interaction.member.permissions.has(component.permissions ?? 0n)) {
-          return interaction.reply({
-            content: `You need the \`${component.permissions}\` permission to use this component`,
-            ephemeral: true
-          });
-        }
+      if (!interaction.guild.me.permissions.has(component.botPermissions ?? 0n)) {
+        return interaction.reply({
+          embeds: [{
+            title: 'Missinq Permissions',
+            description: `I need the \`${component.botPermissions}\` permission to use this component`,
+            color: client.config.color
+          }],
+          ephemeral: true
+        });
+      }
 
-        if (!interaction.guild.me.permissions.has(component.botPermissions ?? 0n)) {
-          return interaction.reply({
-            embeds: [{
-              title: 'Missinq Permissions',
-              description: `I need the \`${component.botPermissions}\` permission to use this component`,
-              color: client.config.color
-            }],
-            ephemeral: true
-          });
-        }
-
-        component.execute(client, interaction);
-        break;
+      component.execute(client, interaction);
     }
   }
 };
