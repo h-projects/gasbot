0e007952b66ec0b4794c818cfc6fc962a60549b5
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2024-05-03 15:30:14 +0100

    feat: support user installations

--- src/commands/removed.ts
+++ src/commands/removed.ts
@@ -1,5 +1,6 @@
 import type { Application } from '#classes';
 import {
+  type APIEmbedField,
   ApplicationCommandType,
   type ChatInputCommandInteraction,
   ContextMenuCommandBuilder,
@@ -11,15 +12,13 @@ export async function onCommand(
   client: Application,
   interaction: ChatInputCommandInteraction<'cached'> | UserContextMenuCommandInteraction<'cached'>
 ) {
-  const member = interaction.options.getMember('user') ?? interaction.member;
-  const user = interaction.options.getUser('user');
-
-  if ((!member && user) || member.user.bot) {
+  const user = interaction.options.getUser('user') ?? interaction.user;
+  if (user.bot) {
     return interaction.reply({
       embeds: [
         {
           title: 'Invalid User',
-          description: 'You need to mention a valid user!',
+          description: "You can't mention a bot 😦",
           color: client.color
         }
       ],
@@ -34,24 +33,34 @@ export async function onCommand(
   const { count: userCount } = await client.prisma.user.upsert({
     select: { count: true },
     where: {
-      id: BigInt(member.user.id)
+      id: BigInt(user.id)
     },
     create: {
-      id: BigInt(member.user.id)
+      id: BigInt(user.id)
     },
     update: {}
   });
 
-  const { count: guildCount } = await client.prisma.guild.upsert({
-    select: { count: true },
-    where: {
-      id: BigInt(interaction.guildId)
-    },
-    create: {
-      id: BigInt(interaction.guildId)
-    },
-    update: {}
-  });
+  const { count: guildCount } = interaction.inGuild()
+    ? await client.prisma.guild.upsert({
+        select: { count: true },
+        where: {
+          id: BigInt(interaction.guildId)
+        },
+        create: {
+          id: BigInt(interaction.guildId)
+        },
+        update: {}
+      })
+    : {
+        count: null
+      };
+
+  const fields: APIEmbedField[] = [{ name: 'User Stats', value: `Removed ${userCount ?? 0} bad letters from ${user}` }];
+
+  if (guildCount !== null) {
+    fields.push({ name: 'Server Stats', value: `Removed ${guildCount ?? 0} bad letters in this server` });
+  }
 
   return interaction.reply({
     embeds: [
@@ -59,10 +68,7 @@ export async function onCommand(
         title: 'Bad Letters Removed',
         color: client.color,
         description: `Removed ${globalCount} bad letters in total`,
-        fields: [
-          { name: 'Server', value: `Removed ${guildCount ?? 0} bad letters in this server` },
-          { name: 'User', value: `Removed ${userCount ?? 0} bad letters from ${member}` }
-        ]
+        fields
       }
     ]
   });
@@ -71,10 +77,8 @@ export async function onCommand(
 export const slashCommandData = new SlashCommandBuilder()
   .setName('removed')
   .setDescription('Check how many bad letters were removed')
-  .setDMPermission(false)
   .addUserOption(option => option.setName('user').setDescription('User to check').setRequired(false));
 
 export const contextMenuCommandData = new ContextMenuCommandBuilder()
   .setName('Removed Count')
-  .setDMPermission(false)
   .setType(ApplicationCommandType.User);
