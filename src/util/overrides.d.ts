12c0b153ae0ef40bbe2eb7a4aa5eaf942a29079d
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2024-06-20 21:47:19 +0100

    remove typescript overrides

--- src/util/overrides.d.ts
+++ src/util/overrides.d.ts
@@ -1,20 +1,4 @@
-// TODO: remove this file when @types/node gets updated to v22 and typescript 5.5 releases
+// TODO: remove this file once @types/node gets updated to v22
 declare module 'node:fs/promises' {
   export function glob(pattern: string): AsyncGenerator<string>;
-}
-
-declare interface ArrayConstructor {
-  fromAsync<T>(
-    iterableOrArrayLike: AsyncIterable<T> | Iterable<T | PromiseLike<T>> | ArrayLike<T | PromiseLike<T>>
-  ): Promise<T[]>;
-
-  fromAsync<T, U>(
-    iterableOrArrayLike: AsyncIterable<T> | Iterable<T> | ArrayLike<T>,
-    mapFn: (value: Awaited<T>) => U,
-    thisArg?: unknown
-  ): Promise<Awaited<U>[]>;
-}
-
-declare interface IterableIterator<T> {
-  toArray(): T[];
 }
