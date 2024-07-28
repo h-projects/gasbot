1de6b58f231a43b7cb30f5b3a32c00bdcf6f9181
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2024-07-28 13:27:13 +0200

    bump `@types/node` to 22.0.0

--- src/util/files.ts
+++ src/util/files.ts
@@ -1,14 +1,14 @@
 import { glob } from 'node:fs/promises';
-import { join } from 'node:path';
+import { basename, join } from 'node:path';
 import { env } from '#env';
 
-const extension = env.NODE_ENV === 'production' ? '*.js' : '*.ts';
+const extension = env.NODE_ENV === 'production' ? '.js' : '.ts';
 
 export function loadDirectory<T>(relativePath: string) {
-  const files = glob(join(import.meta.dirname, relativePath, extension));
+  const files = glob(join(import.meta.dirname, relativePath, `*${extension}`));
 
   return Array.fromAsync(files, async file => ({
-    name: file.split(/\/|\.|\\/).at(-2) as string,
+    name: basename(file, extension),
     data: (await import(`file://${file}`)) as T
   }));
 }
--- src/util/overrides.d.ts
+++
@@ -1,4 +0,0 @@
-// TODO: remove this file once @types/node gets updated to v22
-declare module 'node:fs/promises' {
-  export function glob(pattern: string): AsyncGenerator<string>;
-}
