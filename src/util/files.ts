8abcd771c54bfccd0f44c802a60fb9079ceba230
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2024-07-28 13:30:23 +0200

    refactor: bump node to 22.0.0 (#406)
    * refactor: bump node to 22.0.0
    
    * bump node types
    
    * remove typescript overrides
    
    * bump `@types/node` to 22.0.0

--- src/util/files.ts
+++ src/util/files.ts
@@ -1,30 +1,14 @@
-import { readdir } from 'node:fs/promises';
-import { join } from 'node:path';
+import { glob } from 'node:fs/promises';
+import { basename, join } from 'node:path';
+import { env } from '#env';
 
-export async function loadDirectory<T>(relativePath: string) {
-  const directory = join(import.meta.dirname, relativePath);
+const extension = env.NODE_ENV === 'production' ? '.js' : '.ts';
 
-  const files = await readdir(directory);
+export function loadDirectory<T>(relativePath: string) {
+  const files = glob(join(import.meta.dirname, relativePath, `*${extension}`));
 
-  const result = [];
-
-  for (const file of files) {
-    const loadedFile = await loadFile<T>(directory, file);
-
-    if (loadedFile) {
-      result.push(loadedFile);
-    }
-  }
-
-  return result;
-}
-
-export async function loadFile<T>(directory: string, file: string) {
-  if (!file.endsWith('.js') && !file.endsWith('.ts')) {
-    return;
-  }
-
-  const [name] = file.split('.');
-  const data = (await import(`file://${join(directory, file)}`)) as T;
-  return { name, data };
+  return Array.fromAsync(files, async file => ({
+    name: basename(file, extension),
+    data: (await import(`file://${file}`)) as T
+  }));
 }
--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,6 +1,6 @@
 import process from 'node:process';
+import { styleText } from 'node:util';
 import { env } from '#env';
-import { blue, bold, magenta, red, yellow } from 'yoctocolors';
 
 const date = new Intl.DateTimeFormat('sv', {
   dateStyle: 'short',
@@ -13,22 +13,22 @@ export function debug(...args: unknown[]) {
     return;
   }
 
-  console.error(bold(magenta(`${date.format()} DEBUG`)), ...args);
+  console.error(styleText(['bold', 'magenta'], `${date.format()} DEBUG`), ...args);
 }
 
 export function error(...args: unknown[]) {
-  console.error(bold(red(`${date.format()} ERROR`)), ...args);
+  console.error(styleText(['bold', 'red'], `${date.format()} ERROR`), ...args);
 }
 
 export function fatal(...args: unknown[]) {
-  console.error(bold(red(`${date.format()} FATAL`)), ...args);
+  console.error(styleText(['bold', 'red'], `${date.format()} FATAL`), ...args);
   process.exit(1);
 }
 
 export function log(...args: unknown[]) {
-  console.log(bold(blue(`${date.format()} LOG`)), ...args);
+  console.log(styleText(['bold', 'blue'], `${date.format()} LOG`), ...args);
 }
 
 export function warn(...args: unknown[]) {
-  console.warn(bold(yellow(`${date.format()} WARN`)), ...args);
+  console.warn(styleText(['bold', 'yellow'], `${date.format()} WARN`), ...args);
 }
