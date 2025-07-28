f704ddd5ab33cbec07e42540b78768dc1132ccad
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2025-07-28 20:32:52 +0200

    feat: components v2 in `/chanqeloq`

--- src/commands/changelog.ts
+++ src/commands/changelog.ts
@@ -1,23 +1,49 @@
+import dedent from 'dedent';
 import {
   ApplicationIntegrationType,
   ChatInputCommandBuilder,
   type ChatInputCommandInteraction,
-  InteractionContextType
+  ComponentType,
+  InteractionContextType,
+  MessageFlags
 } from 'discord.js';
 import type { Application } from '#classes';
-import { changelog } from '#util';
+
+const version = '4.1.0';
 
 export function onChatInputCommand(client: Application, interaction: ChatInputCommandInteraction) {
   return interaction.reply({
-    embeds: [
+    flags: MessageFlags.IsComponentsV2,
+    components: [
       {
-        title: 'Chanqeloq',
-        url: 'https://github.com/h-projects/gasbot/blob/main/CHANGELOG.md',
-        author: {
-          name: changelog.version
-        },
-        color: client.color,
-        fields: changelog.features
+        type: ComponentType.Container,
+        accentColor: client.color,
+        components: [
+          {
+            type: ComponentType.TextDisplay,
+            content: `# [${version}](https://github.com/h-projects/gasbot/releases/tag/${version}) Chanqeloq`
+          },
+          {
+            type: ComponentType.Separator
+          },
+          {
+            type: ComponentType.TextDisplay,
+            content: dedent`
+              ### Support for User Installations
+              You can now add the bot to your account and use most commands outside servers
+            `
+          },
+          {
+            type: ComponentType.Separator
+          },
+          {
+            type: ComponentType.TextDisplay,
+            content: dedent`
+              ### Full Infrastructure Upqrade
+              Most internal systems have been revamped and should be more reliable
+            `
+          }
+        ]
       }
     ]
   });
