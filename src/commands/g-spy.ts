a0fe5859e6c1f062c12b66b807cc6e05d7da540c
Madeline Gurriarán <53496941+SuperchupuDev@users.noreply.github.com>
2026-04-30 14:54:08 +0200

    feat: components v2 in `/g-spy`

--- src/commands/g-spy.ts
+++ src/commands/g-spy.ts
@@ -1,18 +1,84 @@
 import {
-  ActionRowBuilder,
   ApplicationIntegrationType,
   type ButtonInteraction,
+  ButtonStyle,
   ChatInputCommandBuilder,
   type ChatInputCommandInteraction,
+  ComponentType,
+  type GuildMember,
   InteractionContextType,
   MessageFlags,
   PermissionFlagsBits,
+  type Role,
   UserContextCommandBuilder,
   type UserContextMenuCommandInteraction
 } from 'discord.js';
 import type { Application } from '#classes';
 
 export const appPermissions = [PermissionFlagsBits.ManageRoles];
+
+const getMissingPermissionsComponent = (client: Application, role: Role) => [
+  {
+    type: ComponentType.Container,
+    accentColor: client.color,
+    components: [
+      {
+        type: ComponentType.TextDisplay,
+        content: '# Missinq Permissions'
+      },
+      {
+        type: ComponentType.Separator
+      },
+      {
+        type: ComponentType.TextDisplay,
+        content: `Make sure ${role} is lower than my hiqhest role`
+      }
+    ]
+  }
+];
+
+const getSuccessComponent = (
+  client: Application,
+  interaction:
+    | ChatInputCommandInteraction<'cached'>
+    | UserContextMenuCommandInteraction<'cached'>
+    | ButtonInteraction<'cached'>,
+  member: GuildMember | null,
+  role?: Role
+) => [
+  {
+    type: ComponentType.Container,
+    accentColor: client.color,
+    components: [
+      {
+        type: ComponentType.TextDisplay,
+        content: '# Done'
+      },
+      {
+        type: ComponentType.Separator
+      },
+      {
+        type: ComponentType.TextDisplay,
+        content: `${member} was marked as a ${role}`
+      },
+      {
+        type: ComponentType.Separator
+      },
+      {
+        type: ComponentType.ActionRow,
+        components: [
+          {
+            type: ComponentType.Button,
+            style: ButtonStyle.Secondary,
+            customId: interaction.isButton() ? interaction.customId : `g-spy:${member?.id}:${interaction.user.id}`,
+            label: interaction.isButton() ? (member ? 'Reverted' : 'Invalid User') : 'Revert',
+            disabled: interaction.isButton()
+          }
+        ]
+      }
+    ]
+  }
+];
 
 export async function onCommand(
   client: Application,
@@ -22,14 +88,26 @@ export async function onCommand(
 
   if (!member || member.id === interaction.user.id || member.user.bot) {
     return interaction.reply({
-      embeds: [
+      components: [
         {
-          title: 'Invalid User',
-          description: 'You need to mention a valid user!',
-          color: client.color
+          type: ComponentType.Container,
+          accentColor: client.color,
+          components: [
+            {
+              type: ComponentType.TextDisplay,
+              content: '# Invalid User'
+            },
+            {
+              type: ComponentType.Separator
+            },
+            {
+              type: ComponentType.TextDisplay,
+              content: "That's not a valid user ☹️"
+            }
+          ]
         }
       ],
-      flags: MessageFlags.Ephemeral
+      flags: MessageFlags.Ephemeral | MessageFlags.IsComponentsV2
     });
   }
 
@@ -42,62 +120,56 @@ export async function onCommand(
 
   if (!role.editable) {
     return interaction.reply({
-      embeds: [
-        {
-          title: 'Missinq Permissions',
-          description: `Make sure ${role} is lower than my hiqhest role`,
-          color: client.color
-        }
-      ],
-      flags: MessageFlags.Ephemeral
+      components: getMissingPermissionsComponent(client, role),
+      flags: MessageFlags.Ephemeral | MessageFlags.IsComponentsV2
     });
   }
 
   await member.roles.add(role);
 
-  const row = new ActionRowBuilder().addSecondaryButtonComponents(button =>
-    button.setLabel('Revert').setCustomId(`g-spy:${member.id}:${interaction.user.id}`)
-  );
-
   return interaction.reply({
-    embeds: [
-      {
-        title: 'Done',
-        description: `${member} was marked as a ${role}`,
-        color: client.color
-      }
-    ],
-    components: [row]
+    flags: MessageFlags.IsComponentsV2,
+    components: getSuccessComponent(client, interaction, member, role),
+    allowedMentions: {
+      parse: []
+    }
   });
 }
 
 export async function onComponent(client: Application, interaction: ButtonInteraction<'cached'>, value: string) {
   const member = await interaction.guild.members.fetch(value).catch(() => null);
   const role = interaction.guild.roles.cache.find(r => r.name === 'g-spy');
-  const label = member ? 'Reverted' : 'Invalid User';
 
   if (role && !role.editable) {
     return interaction.reply({
-      embeds: [
+      components: [
         {
-          title: 'Missinq Permissions',
-          description: `Make sure ${role} is lower than my hiqhest role`,
-          color: client.color
+          type: ComponentType.Container,
+          accentColor: client.color,
+          components: [
+            {
+              type: ComponentType.TextDisplay,
+              content: '# Missinq Permissions'
+            },
+            {
+              type: ComponentType.Separator
+            },
+            {
+              type: ComponentType.TextDisplay,
+              content: `Make sure ${role} is lower than my hiqhest role`
+            }
+          ]
         }
       ],
-      flags: MessageFlags.Ephemeral
+      flags: MessageFlags.Ephemeral | MessageFlags.IsComponentsV2
     });
   }
 
-  await member?.roles.remove(role ?? '');
+  if (role) {
+    await member?.roles.remove(role);
+  }
 
-  const row = new ActionRowBuilder().addSecondaryButtonComponents(button =>
-    button.setLabel(label).setCustomId(interaction.customId).setDisabled(true)
-  );
-
-  return interaction.update({
-    components: [row]
-  });
+  return interaction.update({ components: getSuccessComponent(client, interaction, member, role) });
 }
 
 export const hasComponent = true;
